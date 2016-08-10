142a396675cc5f6e040406a924a954572ac6a740
bergjo06 <bergjo06@luther.edu>
2016-08-10 03:10:55 -0600

    going to bed

--- main.cpp
+++ main.cpp
@@ -15,9 +15,122 @@
 #include <iostream>
 using namespace std;
 
-/*
- * 
- */
+int left(int i)
+{
+    return 2*i;
+}
+
+int right(int i)
+{
+    return (2*i)+1;
+}
+
+int parent(int i)
+{
+    return i/2;
+}
+
+int search(int arr[],int spot, int key)
+{
+    if(arr[spot]==0 || arr[spot]==key)
+    {
+        return spot;
+    }
+    if(key < arr[spot])
+    {
+        return search(arr,left(spot),key);
+    }
+    return search(arr,right(spot),key);
+}
+
+int max(int arr[], int spot)
+{
+    while(arr[right(spot)]!= 0)
+    {
+        spot=right(spot);
+    }
+    return spot;
+}
+
+int min(int arr[], int spot)
+{
+    while(arr[left(spot)]!= 0)
+    {
+        spot=left(spot);
+    }
+    return spot;
+}
+
+
+void insert(int arr[], int val)
+{
+    int y=0;
+    int x=0;
+    while(arr[x]!=0)
+    {
+        y=x;
+        if(val,arr[x])
+        {
+            x=left(x);
+        }
+        else x = right(x);
+    }
+    if(val<arr[y])
+    {
+        arr[left(y)]=val;
+    }
+    else
+    {
+        arr[right(y)]=val;
+    }
+}
+
+void transplant(int arr[], int u, int v)
+{
+    if(parent(u)==0)
+    {
+        arr[0]=v;
+    }
+    else if(u == arr[left(parent(u))])
+    {
+        arr[left(parent(u))]=v;
+    }
+    else
+    {
+        arr[right(parent(u))]=v;
+    }
+    if(v!=0)
+    {
+        arr[parent(v)]=arr[parent(u)];
+    }
+}
+
+void deleter(int arr[],int val)
+{
+    if(arr[left(val)]==0)
+    {
+        transplant(arr,val,right(val));
+    }
+    else if(arr[right(val)]==0)
+    {
+        transplant(arr,val,left(val));
+    }
+    else
+    {
+        int y = min(arr,0);
+        if(parent(y)!=val)
+        {
+            transplant(arr,y,right(y));
+            arr[right(y)]=arr[right(val)];
+            arr[parent(right(y))]=y;
+        }
+        transplant(arr,val,y);
+        arr[left(y)]=arr[left(val)];
+        arr[parent(left(y))]=y;
+    }
+}
+
+
 int main(int argc, char** argv) {
     
     int u = 1000;
@@ -34,6 +147,13 @@ int main(int argc, char** argv) {
         cout << i << " " << d[i] << endl;
     }
     
+    int Mnk[u] {0};
+    
+    int tree[k-1*10000]={};
+    for(int i=0;i<k-1;++i)
+    {
+        insert(tree,d[i]);
+    }
     
     
     
